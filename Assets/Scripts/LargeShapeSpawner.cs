1ef55b229618669d5d898e209f968e319eb22419
Seth Oseghale <setho@Seths-Mac-mini.lan>
2021-06-11 21:45:07 -0400

    Added LargeShapeSpawner logic

--- Assets/Scripts/LargeShapeSpawner.cs
+++ Assets/Scripts/LargeShapeSpawner.cs
@@ -1,13 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LargeShapeSpawner : MonoBehaviour
 {
+    public List<Sprite> largeShapeSprites;
+
+    private void Awake()
+    {
+
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set the random seed
+        Random.InitState(System.DateTime.Now.Millisecond);
+        // Get the count of all large shape sprites
+        int maxShapes = largeShapeSprites.Count;
+        // Get a random number between 1, (count of all large shapes)
+        int randNum = Random.Range(1, maxShapes+1);
+        Debug.Log(string.Format("LargeShapeSpawner::Count is {0} | RandNum is {1}", maxShapes, randNum));
+
+        // Load the large shape of the random number index
+        GetComponent<Image>().sprite = largeShapeSprites[randNum - 1];
+
     }
 
     // Update is called once per frame
